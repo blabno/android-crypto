468229bf96a2119ca6e8d2ed805a3036a3776bdb
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2023-11-05 21:12:32 +0000

    Tweak exception message

--- android-crypto/src/main/java/com/labnoratory/android_crypto/AliasConflictException.java
+++ android-crypto/src/main/java/com/labnoratory/android_crypto/AliasConflictException.java
@@ -3,6 +3,6 @@ package com.labnoratory.android_crypto;
 public class AliasConflictException extends RuntimeException {
 
     public AliasConflictException() {
-        super("Some key for given alias already exists");
+        super("A key for given alias already exists");
     }
 }
